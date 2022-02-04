954facb7b3251276fed75dd20c7b9707ae1aa02e
S@y@li1431997 <sayali.sonawane@mindbowser.com>
2022-02-04 19:45:06 +0530

    Integrated react-native-image-crop-picker

--- App.js
+++ App.js
@@ -6,98 +6,130 @@
  * @flow strict-local
  */
 
-import React from 'react';
-import type {Node} from 'react';
+import React, { useState } from 'react';
 import {
+  Dimensions,
+  FlatList,
   SafeAreaView,
-  ScrollView,
   StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
   View,
+  TouchableOpacity,
+  PermissionsAndroid
 } from 'react-native';
 
 import {
   Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
+import ImagePicker from "react-native-image-crop-picker";
+
+const App = () => {
+
+  const [list, setList] = useState(
+    [
+      { key: "Square" },
+      { key: "Portrait" },
+      { key: "LandScape" },
+    ]
   );
-};
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+  const renderItem = (item ) => (
+    <View style={styles.GridViewContainer}>
+      <Text style={styles.GridViewTextLayout} 
+    //  onPress={this.GetGridViewItem.bind(this, item.key)} 
+      > 
+        {item.key} 
+      </Text>
+   </View> 
+); 
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+async function checkOrientation(img){
+  if (img.width > img.height) {
+    console.log('landscape');
+    await As
+    } else if (img.width < img.height) {
+    console.log('portrait');
+    } else {
+    console.log('even');
+    }
+}
+
+function openImagePicker(type) {
+  if (type === "camera"){
+    ImagePicker.openCamera({
+      cropping: true,
+  })
+      .then((image) => {
+          checkOrientation(image.cropRect);
+          console.log(image);
+      })
+      .catch((error) => {
+          console.log(error);
+      });
+  } else {
+    ImagePicker.openPicker({
+      cropping: true,
+  })
+      .then((image) => {
+        checkOrientation(image.cropRect);
+        console.log(image);
+      })
+      .catch((error) => {
+          console.log(error);
+      });
+  }
+}
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
+    <SafeAreaView style={styles.Container}>
+      <StatusBar barStyle={'dark-content'} backgroundColor={Colors.white} />
         <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+          style={styles.Container}>
+          {/* <Text style={styles.Title}>{"List of Image Categories"}</Text> */}
+          <FlatList
+                  data={list}
+                  renderItem={ ({item}) => renderItem(item)}
+                  keyExtractor={item => item.key}
+                  numColumns={2}
+                />
+                {/* <Image
+          source={{uri: "file:///data/user/0/com.think_bridge/cache/rn_image_picker_lib_temp_46b65a5f-69ef-45a2-b7c2-0ee517112bde.jpg"}}
+          style={styles.imageStyle}
+        />
+        <Text style={styles.textStyle}>{filePath.uri}</Text> */}
+        <TouchableOpacity
+          activeOpacity={0.5}
+          style={styles.buttonStyle}
+          onPress={() => {openImagePicker("camera")}}>
+          <Text style={styles.textStyle}>
+            Launch Camera for Image
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          activeOpacity={0.5}
+          style={styles.buttonStyle}
+          onPress={() => {openImagePicker("gallery")}}>
+          <Text style={styles.textStyle}>Choose Image from Gallery</Text>
+        </TouchableOpacity>
         </View>
-      </ScrollView>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  Container: {
+    flex: 1,
+    backgroundColor: Colors.white,
+    margin:5,
   },
-  sectionTitle: {
+  Title: {
     fontSize: 24,
-    fontWeight: '600',
+    fontWeight: 'bold',
+    fontStyle:'normal',
+    color:"#0A0AFF",
+    marginVertical:10,
   },
   sectionDescription: {
     marginTop: 8,
@@ -106,6 +138,45 @@ const styles = StyleSheet.create({
   },
   highlight: {
     fontWeight: '700',
+  },
+  GridViewContainer: {
+    flex:1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 150,
+    margin: 5,
+    backgroundColor: '#7BBFFF'
+ },
+ GridViewTextLayout: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    justifyContent: 'center',
+    color: '#fffF',
+    padding: 10,
+  },
+  titleText: {
+    fontSize: 22,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    paddingVertical: 20,
+  },
+  textStyle: {
+    padding: 10,
+    color: 'black',
+    textAlign: 'center',
+  },
+  buttonStyle: {
+    alignItems: 'center',
+    alignSelf:'center',
+    backgroundColor: '#DDDDDD',
+    padding: 5,
+    marginVertical: 10,
+    width: 250,
+  },
+  imageStyle: {
+    width: 200,
+    height: 200,
+    margin: 5,
   },
 });
 
